d57e1163f7223f1bb69344ecfe5f35f45c66db8d
unknown <milan_cirkovic@yahoo.com>
2020-10-20 22:43:21 +0200

    Updates

---
+++ precache-manifest.809c67ba58138e6662d64b5fc4667ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db00d3bfd7491865b3fe1e9ade920c4",
+    "url": "/crypto-list/index.html"
+  },
+  {
+    "revision": "9086d9043eecdb648a8d",
+    "url": "/crypto-list/static/css/main.0274a663.chunk.css"
+  },
+  {
+    "revision": "5321ad36046f262e514d",
+    "url": "/crypto-list/static/js/2.9164fe9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crypto-list/static/js/2.9164fe9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9086d9043eecdb648a8d",
+    "url": "/crypto-list/static/js/main.e74cff26.chunk.js"
+  },
+  {
+    "revision": "d29f103896e4e93b5245",
+    "url": "/crypto-list/static/js/runtime-main.6279c661.js"
+  }
+]);
